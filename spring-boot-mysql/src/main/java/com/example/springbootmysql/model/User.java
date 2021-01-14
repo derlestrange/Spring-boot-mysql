e5bf4acb6d81000a041bfca1f607f64fb8d122ed
derlestrange <56831326+derlestrange@users.noreply.github.com>
2021-01-14 23:18:23 +0400

    fix model

--- spring-boot-mysql/src/main/java/com/example/springbootmysql/model/User.java
+++ spring-boot-mysql/src/main/java/com/example/springbootmysql/model/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,20 +12,50 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Entity
-    @Table(name = "roles")
-    public class Role {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        @Column(name = "role_id")
-        private int id;
-        @Column(name = "role")
-        private String role;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(name = "user_name")
+    @Length(min = 5, message = "*Your user name must have at least 5 characters")
+    @NotEmpty(message = "*Please provide a user name")
+    private String userName;
+
+    @Column(name = "email")
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Please provide an email")
+    private String email;
+
+    @Column(name = "password")
+    @Length(min = 5, message = "*Your password must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your password")
+    private String password;
+
+    @Column(name = "name")
+    @NotEmpty(message = "*Please provide your name")
+    private String name;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "*Please provide your last name")
+    private String lastName;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
 }
